e09061a8d08fa9ea77ed890b7e003c353c5f3fab
Janek Kossinski <janekos@users.noreply.github.com>
2017-11-08 16:16:41 +0200

    calories

---
+++ tund/app/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Calories
+
+class CaloriesForm(forms.Form):
+    food_item = forms.CharField(
+        max_length=200,
+        widget=forms.TextInput(attr={
+            'placeholder': 'Toit',
+        }),
+        required=False
+    )
+    calories = forms.IntegerField(
+        widget=forms.NumberInput(attr={
+            'placeholder': 'Kalorid (100g)'
+        }),
+        required=False
+    )
--- tund/app/models.py
+++ tund/app/models.py
@@ -5,3 +5,7 @@ class Tuba(models.Model):
     toa_number = models.IntegerField(default=1)
     kylalise_nimi = models.CharField(max_length=200)
     valjumise_kuupaev = models.DateTimeField(blank=True,null=True)
+
+class Calories(models.Model):
+    food_item = models.CharField(max_length=200)
+    calories = models.IntegerField(default=0)
