a2f551efd59f7c5bfb6fcebca67b25907cecee31
Janek Kossinski <janekos@users.noreply.github.com>
2017-11-08 16:22:25 +0200

    views

--- tund/app/views.py
+++ tund/app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.forms.formsets import formset_factory
+
+from .forms import CaloriesForm
+from .models import Calories
 
 # Create your views here.
 
 def index(request):
     return render(request, 'base.html')
+
+def calories_view(request):
+    form_set = formset_factory(CaloriesForm)
+
+    context = {
+        'calories_formset': form_set,
+        'data': Calories.objects.all()
+    }
+
+    return render(request, 'calories.html', context)
