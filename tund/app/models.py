2e877e583ff027421bb841cc00421efe28b9718a
Janek Kossinski <janekos@users.noreply.github.com>
2017-11-08 17:06:00 +0200

    models

--- tund/app/models.py
+++ tund/app/models.py
@@ -9,3 +9,16 @@ class Tuba(models.Model):
 class Calories(models.Model):
     food_item = models.CharField(max_length=200)
     calories = models.IntegerField(default=0)
+    
+class raamatud(models.Model):
+    raamatu_nr = models.IntegerField(default=0, primary_key=True)
+    raamatu_nimi = models.CharField(max_length=255)
+    raamatu_lehti = models.IntegerField(default=0)
+    omanikud = models.ManyToManyField("omanikud")
+    
+class omanikud(models.Model):
+    id = models.IntegerField(primary_key=True)
+    omaniku_nr = models.IntegerField(default=0)
+    omanik_nimi = models.CharField(max_length=255)
+    omatud_raamatu_nr = models.IntegerField(default=0)
+    
