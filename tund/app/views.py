5fa5d9ad93644ae25c2d1584f5b1201e67909c66
Janek Kossinski <janekos@users.noreply.github.com>
2017-11-08 16:42:55 +0200

    merge
    asd

--- tund/app/views.py
+++ tund/app/views.py
@@ -12,6 +12,15 @@ def index(request):
 def calories_view(request):
     form_set = formset_factory(CaloriesForm)
 
+    if request.method == 'POST' and 'submit_items' in request.POST:
+        calories_formset = form_set(request.POST)
+        if calories_formset.is_valid():
+            data = []
+            for form in calories_formset:
+                food_item = form.cleaned_data.get('food_item')
+                calories = form.cleaned_data.get('calories')
+                if food_item and calories 
+
     context = {
         'calories_formset': form_set,
         'data': Calories.objects.all()
