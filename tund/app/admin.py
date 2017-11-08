1e29dd98941b389b2026d3e173f97241a0656012
Janek Kossinski <janekos@users.noreply.github.com>
2017-11-08 17:38:40 +0200

    admin uuendatud

--- tund/app/admin.py
+++ tund/app/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 admin.site.register(models.Tuba)
+admin.site.register(models.omanikud)
+admin.site.register(models.raamatud)
