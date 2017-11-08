f379fa6926f1a84fda1bffa96c69125cb21013d2
Janek Kossinski <janekos@users.noreply.github.com>
2017-11-08 17:26:22 +0200

    save_raamatud

---
+++ tund/app/utils.py
@@ -0,0 +1,31 @@
+import urllib.request
+from . import models
+
+
+def read_from_file(url):
+    file = urllib.request.urlopen(url)
+    data_bytes = file.read()
+    data_string = data_bytes.decode("utf8")
+    file.close()
+    return data_string
+    
+def save_raamatud(data_string):
+    data = data_string.splitlines()
+    dataLen = len(data)
+    
+    kasKirjeteArvudSamad = (models.raamatud.objects.all().count() >= (dataLen-1))
+    
+    if(not kasKirjeteArvudSamad):
+        for i in range(1, dataLen):
+            attribute = data[i].split(',')
+            model = models.raamatud()
+            omanikeModel = models.omanikud()
+            model.raamatu_nr = attribute[0]
+            model.raamatu_nimi = attribute[1]
+            model.raamatu_lehti = attribute[2]
+            
+            model.save()
+            for item in models.omanikud.objects.all():
+                if(int(item.omatud_raamatu_nr) == int(attribute[0])):
+                    model.omanikud.add(item)
+            
